02d9c439d9a538526b5732754f371a8e148010e9
nikhilsinghgit <nikhilsinghz@yahoo.com>
2022-11-12 13:55:49 +0530

    add files

---
+++ src/main/java/com/naukari/automation/App.java
@@ -0,0 +1,19 @@
+package com.naukari.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println( "Hello World Cup !!" );
+        
+        System.out.println( "Hello World!" );
+        
+        System.out.println( "Hello World Cup !!" );
+    }
+}
